d7a0c3f6aeec2d1b02325467127a2eea430386c8
Amos <amosnistrian@gmail.com>
2018-02-07 12:12:02 -0800

    initial

---
+++ UniversalCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniversalCamera
+//
+//  Created by Amos on 2/7/18.
+//  Copyright © 2018 Amos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
